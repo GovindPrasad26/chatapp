0be420a741ba86b0784518ba83dc3e9771d3ba52
22A91F0013 <danduproluprasad5@gmail.com>
2025-05-15 22:00:15 +0530

    Update main.jsx and vite.config.js

--- chatdude-client/client/src/main.jsx
+++ chatdude-client/client/src/main.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Provider } from "react-redux"; 
 import store from "./reduxstore/store.js";
-
+import { HashRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>  
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
