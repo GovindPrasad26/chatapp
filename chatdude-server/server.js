925dfbe968eae444fa226baf64f9f54f4d197243
22A91F0013 <danduproluprasad5@gmail.com>
2025-05-15 23:08:04 +0530

    Use dynamic PORT from environment variable

--- chatdude-server/server.js
+++ chatdude-server/server.js
@@ -11,3 +11,9 @@ app.use('/chat',secondroute)
   app.listen(port,()=>{
     console.log("server is created",port)
   })
+
+  const PORT = process.env.PORT || 5665;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
